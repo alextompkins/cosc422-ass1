8ff3aa5ca230006420c343b4d4d54f01110ce750
Alex Tompkins <alextompkins3@gmail.com>
2019-08-12 02:08:09 +1200

    Cleanup of Terrain.cpp
    Took 50 minutes

--- Terrain.cpp
+++ Terrain.cpp
@@ -1,11 +1,12 @@
 //  ========================================================================
-//  COSC422: Computer Graphics (2018);  University of Canterbury.
+//  COSC422: Advanced Computer Graphics;  University of Canterbury.
 //
 //  FILE NAME: Terrain.cpp
-//  This is part of Assignment1 files.
+//  AUTHOR: Alex Tompkins
+//  Part II of Assignment #1
 //
 //	The program generates and loads the mesh data for a terrain floor (100 verts, 81 elems).
-//  Required files:  Terrain.vert (vertex shader), Terrain.frag (fragment shader), HeightMap1.tga  (height map)
+//  Required files:  Terrain.vert, Terrain.frag, HeightMap1.tga
 //  ========================================================================
 
 #include <iostream>
@@ -18,110 +19,88 @@
 #include "loadTGA.h"
 using namespace std;
 
-GLuint vaoID;
-GLuint theProgram;
-GLuint mvpMatrixLoc;
+// CONSTANTS
+#define TO_RAD (3.14159265f/180.0f)
 
-float CDR = 3.14159265/180.0;     //Conversion from degrees to rad (required in GLM 0.9.6)
+GLuint terrainProgram;
+GLuint terrainVao;
+GLuint mvpMatrixLoc, textureLoc;
 
-float verts[100*3];       //10x10 grid (100 vertices)
-GLushort elems[81*4];       //Element array for 81 quad patches
+float verts[100 * 3];       //10x10 grid (100 vertices)
+GLushort elems[81 * 4];       //Element array for 81 quad patches
 
 glm::mat4 projView;
 
 //Generate vertex and element data for the terrain floor
-void generateData()
-{
+void generateData() {
 	int indx, start;
 	//verts array
-	for(int i = 0; i < 10; i++)   //100 vertices on a 10x10 grid
+	for (int i = 0; i < 10; i++)   //100 vertices on a 10x10 grid
 	{
-		for(int j = 0; j < 10; j++)
-		{
-			indx = 10*i + j;
-			verts[3*indx] = 10*i - 45;		//x  varies from -45 to +45
-			verts[3*indx+1] = 0;			//y  is set to 0 (ground plane)
-			verts[3*indx+2] = -10*j;		//z  varies from 0 to -100
+		for (int j = 0; j < 10; j++) {
+			indx = 10 * i + j;
+			verts[3 * indx] = 10 * i - 45;        //x  varies from -45 to +45
+			verts[3 * indx + 1] = 0;            //y  is set to 0 (ground plane)
+			verts[3 * indx + 2] = -10 * j;        //z  varies from 0 to -100
 		}
 	}
 
 	//elems array
-	for(int i = 0; i < 9; i++)
-	{
-		for(int j = 0; j < 9; j++)
-		{
-			indx = 9*i +j;
-			start = 10*i + j;
-			elems[4*indx] = start;
-			elems[4*indx+1] = start+10;
-			elems[4*indx+2] = start+11;
-			elems[4*indx+3] = start+1;
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			indx = 9 * i + j;
+			start = 10 * i + j;
+			elems[4 * indx] = start;
+			elems[4 * indx + 1] = start + 10;
+			elems[4 * indx + 2] = start + 11;
+			elems[4 * indx + 3] = start + 1;
 		}
 	}
 }
 
 //Loads terrain texture
-void loadTextures()
-{
+void loadTextures() {
 	GLuint texID;
-    glGenTextures(1, &texID);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texID);
+	glGenTextures(1, &texID);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texID);
 	loadTGA("textures/HeightMap1.tga");
 
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 }
 
-
 //Loads a shader file and returns the reference to a shader object
-GLuint loadShader(GLenum shaderType, string filename)
-{
+GLuint loadShader(GLenum shaderType, string filename) {
 	ifstream shaderFile(filename.c_str());
-	if(!shaderFile.good()) cout << "Error opening shader file." << endl;
+	if (!shaderFile.good()) cout << "Error opening shader file." << endl;
 	stringstream shaderData;
 	shaderData << shaderFile.rdbuf();
 	shaderFile.close();
 	string shaderStr = shaderData.str();
-	const char* shaderTxt = shaderStr.c_str();
+	const char *shaderTxt = shaderStr.c_str();
 
 	GLuint shader = glCreateShader(shaderType);
 	glShaderSource(shader, 1, &shaderTxt, NULL);
 	glCompileShader(shader);
 	GLint status;
 	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-	if (status == GL_FALSE)
-	{
+	if (status == GL_FALSE) {
 		GLint infoLogLength;
 		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
 		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
 		glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
 		const char *strShaderType = NULL;
-		cerr <<  "Compile failure in shader: " << strInfoLog << endl;
+		cerr << "Compile failure in shader: " << strInfoLog << endl;
 		delete[] strInfoLog;
 	}
 	return shader;
 }
 
-//Initialise the shader program, create and load buffer data
-void initialise()
-{
-	glm::mat4 proj, view;   //Projection and view matrices
-//--------Load terrain height map-----------
-	loadTextures();
-//--------Load shaders----------------------
-	GLuint shaderv = loadShader(GL_VERTEX_SHADER, "shaders/Terrain.vert");
-	GLuint shaderf = loadShader(GL_FRAGMENT_SHADER, "shaders/Terrain.frag");
-
-	GLuint program = glCreateProgram();
-	glAttachShader(program, shaderv);
-	glAttachShader(program, shaderf);
-	glLinkProgram(program);
-
+void printLogs(GLuint program) {
 	GLint status;
-	glGetProgramiv (program, GL_LINK_STATUS, &status);
-	if (status == GL_FALSE)
-	{
+	glGetProgramiv(program, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE) {
 		GLint infoLogLength;
 		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
 		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
@@ -129,71 +108,82 @@ void initialise()
 		fprintf(stderr, "Linker failure: %s\n", strInfoLog);
 		delete[] strInfoLog;
 	}
-	glUseProgram(program);
+}
 
-	mvpMatrixLoc = glGetUniformLocation(program, "mvpMatrix");
-	GLuint texLoc = glGetUniformLocation(program, "heightMap");
-	glUniform1i(texLoc, 0);
+//Initialise the shader program, create and load buffer data
+void initialise() {
+	glm::mat4 proj, view;   //Projection and view matrices
+	//--------Load terrain height map-----------
+	loadTextures();
+	//--------Load shaders----------------------
+	GLuint shaderv = loadShader(GL_VERTEX_SHADER, "shaders/Terrain.vert");
+	GLuint shaderf = loadShader(GL_FRAGMENT_SHADER, "shaders/Terrain.frag");
 
-//--------Compute matrices----------------------
-	proj = glm::perspective(30.0f*CDR, 1.25f, 20.0f, 500.0f);  //perspective projection matrix
-	view = glm::lookAt(glm::vec3(0.0, 20.0, 30.0), glm::vec3(0.0, 0.0, -40.0), glm::vec3(0.0, 1.0, 0.0)); //view matrix
+	terrainProgram = glCreateProgram();
+	glAttachShader(terrainProgram, shaderv);
+	glAttachShader(terrainProgram, shaderf);
+	glLinkProgram(terrainProgram);
+	printLogs(terrainProgram);
+
+	glUseProgram(terrainProgram);
+
+	mvpMatrixLoc = glGetUniformLocation(terrainProgram, "mvpMatrix");
+	textureLoc = glGetUniformLocation(terrainProgram, "heightMap");
+	glUniform1i(textureLoc, 0);
+
+	//--------Compute matrices----------------------
+	proj = glm::perspective(30.0f * TO_RAD, 1.25f, 20.0f, 500.0f);  // perspective projection matrix
+	view = glm::lookAt(glm::vec3(0.0, 20.0, 30.0), glm::vec3(0.0, 0.0, -40.0), glm::vec3(0.0, 1.0, 0.0)); // view matrix
 	projView = proj * view;  //Product (mvp) matrix
 
-//---------Load buffer data-----------------------
+	//---------Load buffer data-----------------------
 	generateData();
 
 	GLuint vboID[2];
-	glGenVertexArrays(1, &vaoID);
-    glBindVertexArray(vaoID);
+	glGenVertexArrays(1, &terrainVao);
+	glBindVertexArray(terrainVao);
 
-    glGenBuffers(2, vboID);
+	glGenBuffers(2, vboID);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vboID[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-    glEnableVertexAttribArray(0);  // Vertex position
+	glBindBuffer(GL_ARRAY_BUFFER, vboID[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);  // Vertex position
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboID[1]);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elems), elems, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboID[1]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elems), elems, GL_STATIC_DRAW);
 
-    glBindVertexArray(0);
+	glBindVertexArray(0);
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 }
 
-//Display function to compute uniform values based on transformation parameters and to draw the scene
-void display()
-{
+// Display function to compute uniform values based on transformation parameters and to draw the scene
+void display() {
 	glUniformMatrix4fv(mvpMatrixLoc, 1, GL_FALSE, &projView[0][0]);
 
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-	glBindVertexArray(vaoID);
-	glDrawElements(GL_QUADS, 81*4, GL_UNSIGNED_SHORT, NULL);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glBindVertexArray(terrainVao);
+	glDrawElements(GL_QUADS, 81 * 4, GL_UNSIGNED_SHORT, NULL);
 	glFlush();
 }
 
 
-
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_DEPTH);
 	glutInitWindowSize(1000, 800);
 	glutCreateWindow("Terrain");
-	glutInitContextVersion (4, 2);
-	glutInitContextProfile ( GLUT_CORE_PROFILE );
+	glutInitContextVersion(4, 2);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
 
-	if(glewInit() == GLEW_OK)
-	{
+	if (glewInit() == GLEW_OK) {
 		cout << "GLEW initialization successful! " << endl;
 		cout << " Using GLEW version " << glewGetString(GLEW_VERSION) << endl;
-	}
-	else
-	{
+	} else {
 		cerr << "Unable to initialize GLEW  ...exiting." << endl;
 		exit(EXIT_FAILURE);
 	}
@@ -202,4 +192,3 @@ int main(int argc, char** argv)
 	glutDisplayFunc(display);
 	glutMainLoop();
 }
-
