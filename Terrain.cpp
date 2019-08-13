d6ffa32935de7feb24868dde56d8a5edd9abcc8c
Alex Tompkins <ato47@uclive.ac.nz>
2019-08-13 13:50:45 +1200

    Reformat code to remove tabs

--- Terrain.cpp
+++ Terrain.cpp
@@ -42,80 +42,80 @@ bool wireframeMode = true;
 
 //Generate vertex and element data for the terrain floor
 void generateData() {
-	int indx, start;
-	//verts array
-	for (int i = 0; i < 10; i++) {  //100 vertices on a 10x10 grid
-		for (int j = 0; j < 10; j++) {
-			indx = 10 * i + j;
-			verts[3 * indx] = 10 * i - 45;        //x  varies from -45 to +45
-			verts[3 * indx + 1] = 0;            //y  is set to 0 (ground plane)
-			verts[3 * indx + 2] = -10 * j;        //z  varies from 0 to -100
-		}
-	}
+    int indx, start;
+    //verts array
+    for (int i = 0; i < 10; i++) {  //100 vertices on a 10x10 grid
+        for (int j = 0; j < 10; j++) {
+            indx = 10 * i + j;
+            verts[3 * indx] = 10 * i - 45;        //x  varies from -45 to +45
+            verts[3 * indx + 1] = 0;            //y  is set to 0 (ground plane)
+            verts[3 * indx + 2] = -10 * j;        //z  varies from 0 to -100
+        }
+    }
 
-	//elems array
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			indx = 9 * i + j;
-			start = 10 * i + j;
-			elems[4 * indx] = start;
-			elems[4 * indx + 1] = start + 10;
-			elems[4 * indx + 2] = start + 11;
-			elems[4 * indx + 3] = start + 1;
-		}
-	}
+    //elems array
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            indx = 9 * i + j;
+            start = 10 * i + j;
+            elems[4 * indx] = start;
+            elems[4 * indx + 1] = start + 10;
+            elems[4 * indx + 2] = start + 11;
+            elems[4 * indx + 3] = start + 1;
+        }
+    }
 }
 
 // Loads terrain texture
 void loadTextures() {
-	GLuint texID;
-	glGenTextures(1, &texID);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texID);
-	loadTGA("textures/HeightMap1.tga");
+    GLuint texID;
+    glGenTextures(1, &texID);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texID);
+    loadTGA("textures/HeightMap1.tga");
 
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 }
 
 //Loads a shader file and returns the reference to a shader object
 GLuint loadShader(GLenum shaderType, string filename) {
-	ifstream shaderFile(filename.c_str());
-	if (!shaderFile.good()) cout << "Error opening shader file." << endl;
-	stringstream shaderData;
-	shaderData << shaderFile.rdbuf();
-	shaderFile.close();
-	string shaderStr = shaderData.str();
-	const char *shaderTxt = shaderStr.c_str();
+    ifstream shaderFile(filename.c_str());
+    if (!shaderFile.good()) cout << "Error opening shader file." << endl;
+    stringstream shaderData;
+    shaderData << shaderFile.rdbuf();
+    shaderFile.close();
+    string shaderStr = shaderData.str();
+    const char *shaderTxt = shaderStr.c_str();
 
-	GLuint shader = glCreateShader(shaderType);
-	glShaderSource(shader, 1, &shaderTxt, NULL);
-	glCompileShader(shader);
-	GLint status;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-	if (status == GL_FALSE) {
-		GLint infoLogLength;
-		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
-		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
-		glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
-		const char *strShaderType = NULL;
-		cerr << "Compile failure in shader: " << strInfoLog << endl;
-		delete[] strInfoLog;
-	}
-	return shader;
+    GLuint shader = glCreateShader(shaderType);
+    glShaderSource(shader, 1, &shaderTxt, NULL);
+    glCompileShader(shader);
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE) {
+        GLint infoLogLength;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+        glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
+        const char *strShaderType = NULL;
+        cerr << "Compile failure in shader: " << strInfoLog << endl;
+        delete[] strInfoLog;
+    }
+    return shader;
 }
 
 void printLogs(GLuint program) {
-	GLint status;
-	glGetProgramiv(program, GL_LINK_STATUS, &status);
-	if (status == GL_FALSE) {
-		GLint infoLogLength;
-		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
-		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
-		glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
-		fprintf(stderr, "Linker failure: %s\n", strInfoLog);
-		delete[] strInfoLog;
-	}
+    GLint status;
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE) {
+        GLint infoLogLength;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+        glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
+        fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+        delete[] strInfoLog;
+    }
 }
 
 void setupTerrainProgram() {
@@ -177,18 +177,20 @@ void initialise() {
     setupTerrainProgram();
 
     // Load textures
-	loadTextures();
-	glUniform1i(heightMapperLoc, 0);
+    loadTextures();
+    glUniform1i(heightMapperLoc, 0);
 
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glPatchParameteri(GL_PATCH_VERTICES, 4);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glPatchParameteri(GL_PATCH_VERTICES, 4);
 }
 
 void calcUniforms() {
     glm::mat4 proj = glm::perspective(30.0f * TO_RAD, 1.25f, 0.001f, 10000.0f);  // perspective projection matrix
     glm::mat4 view = glm::lookAt(
             glm::vec3(eyePos.x, eyePos.y, eyePos.z), // eye pos
-            glm::vec3(eyePos.x + cos(glm::radians(eyePos.horAngle)), eyePos.y + sin(glm::radians(eyePos.vertAngle)), eyePos.z + sin(glm::radians(eyePos.horAngle))), // look at pos
+            glm::vec3(eyePos.x + cos(glm::radians(eyePos.horAngle)),
+                      eyePos.y + sin(glm::radians(eyePos.vertAngle)),
+                      eyePos.z + sin(glm::radians(eyePos.horAngle))), // look at pos
             glm::vec3(0.0, 1.0, 0.0)); // up vector
     glm::mat4 mvMatrix = view;
 
@@ -223,11 +225,11 @@ void display() {
     glUseProgram(terrainProgram);
     calcUniforms();
     setPolygonMode(wireframeMode);
-	glBindVertexArray(terrainVao);
-	glDrawElements(GL_PATCHES, 81 * 4, GL_UNSIGNED_SHORT, NULL);
+    glBindVertexArray(terrainVao);
+    glDrawElements(GL_PATCHES, 81 * 4, GL_UNSIGNED_SHORT, NULL);
 
-	glBindVertexArray(0);
-	glFlush();
+    glBindVertexArray(0);
+    glFlush();
 }
 
 void special(int key, int x, int y) {
@@ -275,26 +277,25 @@ void keyboard(unsigned char key, int x, int y) {
     glutPostRedisplay();
 }
 
-
 int main(int argc, char **argv) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_DEPTH);
-	glutInitWindowSize(1000, 800);
-	glutCreateWindow("Terrain");
-	glutInitContextVersion(4, 2);
-	glutInitContextProfile(GLUT_CORE_PROFILE);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_DEPTH);
+    glutInitWindowSize(1000, 800);
+    glutCreateWindow("Terrain");
+    glutInitContextVersion(4, 2);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
 
-	if (glewInit() == GLEW_OK) {
-		cout << "GLEW initialization successful! " << endl;
-		cout << " Using GLEW version " << glewGetString(GLEW_VERSION) << endl;
-	} else {
-		cerr << "Unable to initialize GLEW  ...exiting." << endl;
-		exit(EXIT_FAILURE);
-	}
+    if (glewInit() == GLEW_OK) {
+        cout << "GLEW initialization successful! " << endl;
+        cout << " Using GLEW version " << glewGetString(GLEW_VERSION) << endl;
+    } else {
+        cerr << "Unable to initialize GLEW  ...exiting." << endl;
+        exit(EXIT_FAILURE);
+    }
 
-	initialise();
-	glutSpecialFunc(special);
-	glutKeyboardFunc(keyboard);
-	glutDisplayFunc(display);
-	glutMainLoop();
+    initialise();
+    glutSpecialFunc(special);
+    glutKeyboardFunc(keyboard);
+    glutDisplayFunc(display);
+    glutMainLoop();
 }
